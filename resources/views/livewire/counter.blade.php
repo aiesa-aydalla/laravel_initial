874593c8c091e6e03efc91ada967a7b0b729bdaf
aiesa-aydalla <ajf.aydalla03@gmail.com>
2025-06-25 16:22:41 +0800

    aiesa & naomi

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,16 @@
+<div class="flex p-10 items-center flex-col">
+    
+    <h1 class="text-2xl font-bold mb-4">{{$number}}</h1>
+    
+    <button wire:click="decrement" class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+    <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+    -
+    </span>
+    </button>
+
+   <button wire:click="increment" class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+    <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+    +
+    </span>
+    </button>
+</div>
